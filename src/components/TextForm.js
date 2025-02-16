7234daff46fe0c7d3efd4f7fba665bef6bd5365a
ravirv17 <ravirv1717@gmail.com>
2025-02-17 03:03:04 +0530

    added "your text summary"

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,12 +11,21 @@ export default function TextForm(props) {
     const[text,setText]=useState("Enter your text");
     
   return (
-    <div>
+    <>
+    <div className="container">
         <div className="mb-3">
             <label htmlFor="myBox" className="form-label">{props.title}</label>
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
     </div>
+    <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters.</p>
+        <p>{0.008*text.split(" ").length} minutes read.</p>
+        <h4>Preview</h4>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
